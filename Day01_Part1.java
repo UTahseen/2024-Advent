6e60f560bf0cebeedef16aa5a6bf95dedde26354
UTahseen <tahseenu@nycstudents.net>
2024-12-03 14:25:18 -0500

    Add files via upload

--- Day01_Part1.java
+++ Day01_Part1.java
@@ -7,10 +7,38 @@ public class Day01_Part1 {
     public static void main(String[] args) {
 
         ArrayList<String> fileData = getFileData("src/Day1Input.txt");
-        int difference = 0;
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
         for (int i = 0; i < fileData.size(); i++){
-            String[] sides = fileData.get(i).split(" ");
-            difference += Math.abs(Integer.parseInt(sides[0]) - Integer.parseInt(sides[3]));
+            String[] sides = fileData.get(i).split("   ");
+            left.add(Integer.parseInt(sides[0]));
+            right.add(Integer.parseInt(sides[1]));
+        }
+        for (int i = 0; i < left.size(); i++){
+            int leftSmallest = Integer.MAX_VALUE;
+            int rightSmallest = Integer.MAX_VALUE;
+            int leftIndex = i;
+            int rightIndex = i;
+            for (int j = i; j < left.size(); j++){
+                if (left.get(j) < leftSmallest){
+                    leftSmallest = left.get(j);
+                    leftIndex = j;
+                }
+                if (right.get(j) < rightSmallest){
+                    rightSmallest = right.get(j);
+                    rightIndex = j;
+                }
+            }
+            int holder = left.get(i);
+            left.set(i, leftSmallest);
+            left.set(leftIndex, holder);
+            holder = right.get(i);
+            right.set(i, rightSmallest);
+            right.set(rightIndex, holder);
+        }
+        int difference = 0;
+        for (int i = 0; i < left.size(); i++){
+            difference += Math.abs(left.get(i) - right.get(i));
         }
         System.out.println(difference);
     }
