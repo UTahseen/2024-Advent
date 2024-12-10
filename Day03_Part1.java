1e9906f79baacfa31b09f71147bb2bf595422515
UTahseen <tahseenu@nycstudents.net>
2024-12-10 14:49:46 -0500

    Add files via upload

--- Day03_Part1.java
+++ Day03_Part1.java
@@ -2,12 +2,63 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 public class Day03_Part1 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Day3Input.txt");
-
-
+        String input = "";
+        for (int i = 0; i < fileData.size(); i++){
+            input += fileData.get(i);
+        }
+        ArrayList<String> allMatches = new ArrayList<String>();
+        String regex = "mul\\([1-9],[1-9]\\)";
+        Matcher m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9],[1-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9],[1-9][0-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9],[1-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9],[1-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9],[1-9][0-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9][0-9],[1-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9][0-9],[1-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        regex = "mul\\([1-9][0-9][0-9],[1-9][0-9][0-9]\\)";
+        m = Pattern.compile(regex).matcher(input);
+        while (m.find()) {
+            allMatches.add(m.group());
+        }
+        System.out.println(allMatches);
     }
 
     public static ArrayList<String> getFileData(String fileName) {
