eb11adfff70b65ef4aa05899159116984bf6fd33
UTahseen <tahseenu@nycstudents.net>
2024-12-16 14:51:28 -0500

    Add files via upload

---
+++ Day05_Part1.java
@@ -0,0 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Day05_Part1 {
+    public static void main(String[] args) {
+        ArrayList<String> fileData = getFileData("src/Day5Input.txt");
+        ArrayList<String> rules = new ArrayList<>();
+        ArrayList<String> pages = new ArrayList<>();
+        for (String str : fileData){
+            if (str.indexOf("|") >= 0) rules.add(str);
+            else pages.add (str);
+        }
+        int sum = 0;
+        for (String str : pages){
+            boolean good = true;
+            for (int i = 0; i < rules.size(); i++){
+                String[] values = rules.get(i).split("|");
+                if (str.indexOf(values[2]) >= 0 && str.indexOf(values[0]) > str.indexOf(values[2])){
+                    good = false;
+                    break;
+                }
+            }
+            if (good){
+                String[] updates = str.split (",");
+                sum += Integer.parseInt(updates[updates.length / 2 - 1]);
+            }
+        }
+        System.out.println(sum);
+        // x < 11197
+    }
+
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<String>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
