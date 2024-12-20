dd63c57d4a018f630b5fe3b2debde8f2f3d58278
UTahseen <tahseenu@nycstudents.net>
2024-12-20 14:50:57 -0500

    Add files via upload

--- Day06_Part1.java
+++ Day06_Part1.java
@@ -6,7 +6,75 @@ import java.util.Scanner;
 public class Day06_Part1 {
     public static void main(String[] args) {
         ArrayList<String> fileData = getFileData("src/Day6Input.txt");
-        char[][] map = new char[fileData.size()][fileData.get(0).length()];
+        String[][] map = new String[fileData.size()][fileData.get(0).length()];
+        boolean onMap = true;
+        int row = 0;
+        int col = 0;
+        for (int r = 0; r < map.length; r++){
+            for (int c = 0; c < map[r].length; c++){
+                map[r][c] = fileData.get(r).substring(c, c+1);
+                if (map[r][c].equals("^") || map[r][c].equals("v") || map[r][c].equals("<")|| map[r][c].equals(">")) {
+                    row = r;
+                    col = c;
+                }
+            }
+        }
+        while (onMap){
+            if (map[row][col].equals("^")){
+                while (!map[row-1][col].equals("#") && onMap){
+                    map[row][col] = "X";
+                    map[row][col+1] = "^";
+                    col++;
+                    if (col == map[row].length-1){
+                        map[row][col] = "X";
+                        onMap = false;
+                    }
+                }
+                if (onMap) map[row][col] = "^";
+            }
+            else if (map[row][col].equals(">")){
+                while (!map[row][col+1].equals("#") && onMap){
+                    map[row][col] = "X";
+                    map[row][col+1] = "^";
+                    col++;
+                    if (col == map[row].length-1){
+                        map[row][col] = "X";
+                        onMap = false;
+                    }
+                }
+                if (onMap) map[row][col] = "^";
+            }
+            else if (map[row][col].equals("v")){
+                while (!map[row+1][col].equals("#") && onMap){
+                    map[row][col] = "X";
+                    map[row+1][col] = "^";
+                    row++;
+                    if (row == map.length-1){
+                        map[row][col] = "X";
+                        onMap = false;
+                    }
+                }
+                if (onMap) map[row][col] = "<";
+            }
+            else if (map[row][col].equals("<")){
+                while (!map[row][col-1].equals("#") && onMap){
+                    map[row][col] = "X";
+                    map[row][col-1] = "<";
+                    col--;
+                    if (col == 0){
+                        map[row][col] = "X";
+                        onMap = false;
+                    }
+                }
+                if (onMap) map[row][col] = "^";
+            }
+        }
+        for (int r = 0; r < map.length; r++){
+            for (int c = 0; c < map[r].length; c++){
+                System.out.print(map[r][c]);
+            }
+            System.out.println();
+        }
     }
 
     public static ArrayList<String> getFileData(String fileName) {
